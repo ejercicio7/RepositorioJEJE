0c897c1a0759ef5ea7edf18ba9ddfe1ed48da368
Benito <bocanegraedu0@gmail.com>
2019-05-22 12:32:44 +0200

    Subo la clase persona

---
+++ eclipse-workspace/ProyectoEntornos/src/Persona.java
@@ -0,0 +1,38 @@
+
+public class Persona {
+	private String nombre;
+	private String dni;
+	private String edad;
+	
+
+	public Persona(String nombre, String dni, String edad) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.edad = edad;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	
+}
